67c3136235419de6dae0072e6e5c8c3aa66e3f90
tnkcoll <theodore.collins2023@outlook.com>
2024-06-05 13:56:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment5._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 21;
+            Console.WriteLine(DistinctWaysToClimbStairs(steps));
+        }
+
+        static int DistinctWaysToClimbStairs(int steps)
+        {
+            if (steps <= 1)
+            {
+                return 1;
+            }
+
+            return DistinctWaysToClimbStairs(steps - 1) + DistinctWaysToClimbStairs(steps - 2);
+        }
+    }
+}
